188c140c81a3b6058291f5731f7e39ef7c010ccb
SAID MOTYA <zonek.app@hotmail.com>
2022-01-10 14:22:18 +0100

    This lib for member of secretGFX group

---
+++ app/src/main/java/com/gfx/adPromote/ads/GFX.java
@@ -0,0 +1,13 @@
+package com.gfx.adPromote.ads;
+
+import android.util.Log;
+
+public class GFX {
+
+    public static String AdPromoteLink = "https://raw.githubusercontent.com/saidmotya/MyPromoteFile/main/MyPromote.json";
+
+    public static void setLog(String log){
+        Log.d("gfx",log);
+    }
+
+}
