5f322aabe0920d8371a6ba2c57790a6604ad7497
SAID MOTYA <zonek.app@hotmail.com>
2022-01-10 14:26:03 +0100

    Update GFX.java

--- app/src/main/java/com/gfx/adPromote/ads/GFX.java
+++ app/src/main/java/com/gfx/adPromote/ads/GFX.java
@@ -4,7 +4,7 @@ import android.util.Log;
 
 public class GFX {
 
-    public static String AdPromoteLink = "https://raw.githubusercontent.com/saidmotya/MyPromoteFile/main/MyPromote.json";
+    public static String AdPromoteLink = "https://raw.githubusercontent.com/saidmotya/GFX-AdPromote/master/ScreenShot/MyPromote.json";
 
     public static void setLog(String log){
         Log.d("gfx",log);
